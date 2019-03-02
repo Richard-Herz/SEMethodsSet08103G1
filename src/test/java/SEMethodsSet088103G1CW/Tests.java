c738ce0822bb321be30d103f40ac95a52e558b40
Calder-MacLean <caldermac08@gmail.com>
2019-03-02 14:23:24 +0000

    Started to add methods for tests

---
+++ src/test/java/SEMethodsSet088103G1CW/Tests.java
@@ -0,0 +1,26 @@
+package SEMethodsSet088103G1CW;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Tests
+{
+    static MainWin app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new MainWin();
+    }
+
+    @Test
+    void printPopulationNull()
+    {
+        //MainWin.printSalaries(null);
+    }
+}
